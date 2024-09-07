81491d6ca756bcf4bf84e21e18c4f297508a7f99
Yash Vaghela <yashvaghela432@gmail.com>
2024-09-07 08:55:54 +0000

    code for calculating mean_var_std and more

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,20 @@
 import numpy as np
 
 def calculate(list):
-
-
-
+    list = np.array(list)
+    
+    if len(list) != 9:
+        raise ValueError("List must contain nine numbers.")
+    
+    list = np.reshape(list, (3, 3))
+    
+    calculations = {
+        'mean': [list.mean(axis=0).tolist(), list.mean(axis=1).tolist(), list.mean()],
+        'variance': [list.var(axis=0).tolist, list.var(axis=1).tolist, list.var()],
+        'standard deviation': [list.std(axis=0).tolist, list.std(axis=1).tolist, list.std()],
+        'max': [list.max(axis=0).tolist, list.max(axis=1).tolist, list.max()],
+        'min': [list.min(axis=0).tolist, list.min(axis=1).tolist, list.min()],
+        'sum': [list.sum(axis=0).tolist, list.sum(axis=1).tolist, list.sum()]
+    }
 
     return calculations
